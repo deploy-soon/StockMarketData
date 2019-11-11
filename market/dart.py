129e8f8c1a3fa5031f0cbe45e502411c34153cf2
soon <dyd3312@gmail.com>
2019-11-11 23:19:29 +0900

    crawling dart main page

---
+++ market/dart.py
@@ -0,0 +1,107 @@
+import os
+import sys
+from os.path import join as pjoin
+import fire
+import time
+import tqdm
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import requests
+
+sys.path.append("../tools")
+from misc import get_logger
+from login import Status
+
+
+SATURDAY = 6
+SUNDAY = 7
+
+class DART:
+
+    def __init__(self):
+        self.logger = get_logger()
+        self.root = "http://dart.fss.or.kr"
+        self.from_date = datetime.now()
+        self.to_date = datetime.now() - timedelta(days=10)
+
+    def _check_page_valid(self, trs):
+        try:
+            if len(trs) < 2:
+                self.logger.info("INVALID TABLE")
+                return False
+            if trs[1].find("td", class_="no_data"):
+                self.logger.info("NO DATA PAGE")
+                return False
+            return True
+        except:
+            self.logger.info("INVALID PAGE")
+            return False
+
+    def _extract_tuple(self, tr):
+        try:
+            tds = tr.find_all("td")
+            time = tds[0].text.split(":")
+            hour = int(time[0])
+            minute = int(time[1])
+            company_a = tds[1].find('a')
+            company_href = company_a.get("href")
+            company_id = company_href.split("=")[-1]
+            company_name = company_a.text.strip()
+            report = tds[2].find('a')
+            report_href = report.get('href')
+            report_title = report.text.strip()
+            report_date = tds[4].text.split(".")
+            year = int(report_date[0])
+            month = int(report_date[1])
+            day = int(report_date[2])
+            return {
+                "title": report_title,
+                "href": report_href,
+                "company_name": company_name,
+                "company_id": company_id,
+                "datetime": datetime(year=year, month=month, day=day,
+                                     hour=hour, minute=minute)
+            }
+
+        except:
+            self.logger.info("INVALID TUPLE")
+            return {}
+
+    def get_main_page(self):
+        report_list = []
+
+        url = "{}/dsac001/mainAll.do".format(self.root)
+
+        pivot_date = self.from_date
+        while pivot_date >= self.to_date:
+            if pivot_date.weekday() == SATURDAY or pivot_date.weekday() == SUNDAY:
+                pivot_date = pivot_date - timedelta(days=1)
+                continue
+
+            self.logger.debug("START GET {}".format(pivot_date.strftime("%Y.%m.%d")))
+            params = {
+                "selectDate": pivot_date.strftime("%Y%m%d"),
+                "maxResults": 500,
+                "currentPage": 1
+            }
+            while True:
+                r = requests.get(url, params=params)
+                self.logger.debug(r.url)
+                if r.status_code != 200:
+                    break
+                soup = BeautifulSoup(r.content, "html.parser")
+                trs = soup.find_all('tr')
+                if not self._check_page_valid(trs):
+                    break
+                for tr in trs[1:]:
+                    report_list.append(self._extract_tuple(tr))
+                params["currentPage"] = params["currentPage"] + 1
+                time.sleep(2)
+            pivot_date = pivot_date - timedelta(days=1)
+
+        self.logger.info(len(report_list))
+
+
+if __name__ == "__main__":
+    d = DART()
+    d.get_main_page()
