e2a230f46e5bb4778b88d8a921d2f1c41815a22e
soon <dyd3312@gmail.com>
2019-11-25 13:18:12 +0900

    typo

--- tools/misc.py
+++ tools/misc.py
@@ -4,9 +4,9 @@ def get_logger(name=__file__):
     logger = logging.getLogger(name)
     if logger.handlers:
         return logger
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     sh = logging.FileHandler('./crawling.log')
-    sh.setLevel(logging.INFO)
+    sh.setLevel(logging.DEBUG)
     formatter = logging.Formatter('[%(levelname)-8s] %(asctime)s [%(filename)s] [%(funcName)s:%(lineno)d] %(message)s', '%Y-%m-%d %H:%M:%S')
     sh.setFormatter(formatter)
 
