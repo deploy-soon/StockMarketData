90ca95eaf04f79bdcbd05d97ee24810f2a0f5b06
soon <dyd3312@gmail.com>
2019-11-27 16:12:41 +0900

    Paid-in capital increase report added

--- market/dart_report.py
+++ market/dart_report.py
@@ -32,15 +32,15 @@ class Report:
     def check_report_valid(self, title):
         raise NotImplemented
 
-    def get_document(self, rcpNo, dcmNo):
+    def get_document(self, params):
         url = self.root + "/report/viewer.do"
         params = {
-            "rcpNo": rcpNo,
-            "dcmNo": dcmNo,
-            "eleId": 0,
-            "offset": 0,
-            "length": 0,
-            "dtd": "HTML"
+            "rcpNo": params[0],
+            "dcmNo": params[1],
+            "eleId": 0 if params[2] == 'null' else params[2],
+            "offset": 0 if params[3] == 'null' else params[3],
+            "length": 0 if params[4] == 'null' else params[4],
+            "dtd": params[5],
         }
         r = requests.get(url, params=params)
         self.logger.debug(r.url)
@@ -67,8 +67,8 @@ class Report:
                 continue
             params = [self._clean_data(value) for value in params]
             if params[0].isdigit() and params[1].isdigit():
-                return params[0], params[1]
-        return None, None
+                return params
+        return []
 
     def get_report(self, href):
         url = self.root + href
@@ -77,11 +77,11 @@ class Report:
         if r.status_code != 200:
             self.logger.warning("GET error: {}".format(r.url))
             return None
-        rcpNo, dcmNo = self._get_report_document(str(r.content))
-        if rcpNo is None or dcmNo is None:
+        params = self._get_report_document(r.text)
+        if not params:
             return None
 
-        return self.get_document(rcpNo, dcmNo)
+        return self.get_document(params)
 
     def save(self, results):
 
@@ -92,7 +92,6 @@ class Report:
             writer.writeheader()
             for result in results:
                 writer.writerow(result)
-
 
     def run(self):
         results = []
@@ -149,8 +148,52 @@ class Danil(Report):
                 data[key] = value
         return data
 
+class Usang(Report):
+
+    def __init__(self):
+        Report.__init__(self)
+        self.res_file = pjoin(self.res_path, "usang.csv")
+
+    def check_report_valid(self, title):
+        if not title:
+            return False
+        return "유상증자결정" in title
+
+    def parse_document(self, content):
+        soup = BeautifulSoup(content, "html.parser")
+        trs = soup.find_all('tr')
+        data = {
+            "facility_fund": 0,
+            "operation_fund": 0,
+            "acquisition_fund": 0,
+            "guitar_fund": 0,
+        }
+        for tr in trs:
+            tds = tr.find_all("td")
+            key, value = None, None
+            for td in tds:
+                row = td.text.strip() or ""
+                if "시설자금" in row:
+                    key = "facility_fund"
+                elif "운영자금" in row:
+                    key = "operation_fund"
+                elif "취득자금" in row:
+                    key = "acquisition_fund"
+                elif "기타자금" in row:
+                    key = "guitar_fund"
+                elif row.replace(",", "").isdigit():
+                    value = row.replace(",", "")
+                    if value == "0":
+                        value = None
+                    if len(tds) > 2 and "시설자금" not in row:
+                        value = None
+            if key is not None and value is not None:
+                data[key] = value
+        return data
+
 
 if __name__ == "__main__":
     fire.Fire({
         "Danil": Danil,
+        "Usang": Usang,
     })
