20fea731fe41667622e0c2bb1293d9caccd4e1e2
soon <dyd3312@gmail.com>
2020-02-16 01:39:27 +0900

    remove ETF, sort stocks with daily volume

--- market/ninetoten.py
+++ market/ninetoten.py
@@ -24,6 +24,10 @@ class NinetoTen(Status):
         stockcodes = []
         self.stocknames = {}
         for i, code in enumerate(codeList + codeList2):
+            section = self.stockcode.GetStockSectionKind(code)
+            if section != 1:
+                # not corporation such as ETF, futures, ...
+                continue
             name = self.stockcode.CodeToName(code)
             stockcodes.append(code)
             self.stocknames[code] = name
@@ -63,7 +67,7 @@ class NinetoTen(Status):
                 if self.stock_chart.GetDataValue(0, i) not in pivots:
                     continue
                 minute = self.stock_chart.GetDataValue(1, i)
-                if 901 <= minute and minute < 1030:
+                if 901 <= minute and minute < 1130:
                     candle.setdefault("dates", []).append(self.stock_chart.GetDataValue(0, i))
                     candle.setdefault("minutes", []).append(self.stock_chart.GetDataValue(1, i))
                     candle.setdefault("opens", []).append(self.stock_chart.GetDataValue(2, i))
@@ -114,12 +118,14 @@ class NinetoTen(Status):
             length = self.stock_chart.GetHeaderValue(3)
         return dates, minutes, prices, volumes
 
-    def get_volume(self, stockcode):
+    def get_volume(self, stockcode, time_mode="D"):
+        assert time_mode in ["D", "W", "M", "m", "T"], "invalid time mode"
+
         self.stock_chart.SetInputValue(0, stockcode)
         self.stock_chart.SetInputValue(1, ord('2'))
         self.stock_chart.SetInputValue(4, 100000)
-        self.stock_chart.SetInputValue(5, [0, 1, 8, 13])
-        self.stock_chart.SetInputValue(6, ord("m"))
+        self.stock_chart.SetInputValue(5, [0, 8])
+        self.stock_chart.SetInputValue(6, ord(time_mode))
         self.stock_chart.SetInputValue(9, ord('1'))
         self.stock_chart.BlockRequest()
         length = self.stock_chart.GetHeaderValue(3)
@@ -128,9 +134,8 @@ class NinetoTen(Status):
             for i in range(length):
                 if self.opt.startdate > self.stock_chart.GetDataValue(0, i):
                     break
-                if self.stock_chart.GetDataValue(1, i) == 901:
-                    dates.append(self.stock_chart.GetDataValue(0, i))
-                    volumes.append(self.stock_chart.GetDataValue(2, i))
+                dates.append(self.stock_chart.GetDataValue(0, i))
+                volumes.append(self.stock_chart.GetDataValue(1, i))
             if self.opt.startdate > self.stock_chart.GetDataValue(0, 0):
                 break
             if self.status.getLimitRemainCount(1) < 2:
@@ -138,19 +143,32 @@ class NinetoTen(Status):
             self.stock_chart.BlockRequest()
             self.log_request()
             length = self.stock_chart.GetHeaderValue(3)
+
+        assert len(dates) == len(volumes)
         return dates, volumes
 
     def get_high_volume(self):
+        """
+        get high volume stocks between opt.fromdate to opt.todate
+        :return: {
+            "20200101": [code1, code2, ...],
+            ...
+        }
+        """
         stockcodes = self.get_stockcode()
         stockdata, dateset = dict(), set()
         for stockcode in tqdm.tqdm(stockcodes):
             dates, volumes = self.get_volume(stockcode)
-            stockdata[stockcode] = {date: volume for date, volume in zip(dates, volumes)}
+            stockdata[stockcode] = {int(date): volume for date, volume in zip(dates, volumes)}
             dateset |= set(dates)
+        self.logger.info("get dates from {} to {}".format(min(dateset), max(dateset)))
+
         high_volume = {}
         for date in dateset:
+            if date < self.opt.fromdate or self.opt.todate < date:
+                continue
             price = [(stockcode, series[date]) for stockcode, series in stockdata.items() if date in series]
-            price = sorted(price, key=lambda x:x[1], reverse=True)
+            price = sorted(price, key=lambda x: x[1], reverse=True)
             high_volume[date] = [stockcode for stockcode, p in price[:self.opt.top_volume]]
         return high_volume
 
@@ -158,12 +176,12 @@ class NinetoTen(Status):
         stockcodes = []
         self.logger.info("Extract minute data : {} stocks".format(len(stock_map.keys())))
         with h5py.File(pjoin(self.opt.export_to, "ninetoten.h5"), "w") as fout:
-            for key, value in tqdm.tqdm(stock_map.items()):
-                candle = self.get_minute_data(key, value)
+            for stockcode, datelist in tqdm.tqdm(stock_map.items()):
+                candle = self.get_minute_data(stockcode, datelist)
                 if not candle:
                     continue
-                stockcodes.append(key)
-                stockgroup = fout.create_group(key)
+                stockcodes.append(stockcode)
+                stockgroup = fout.create_group(stockcode)
                 stockgroup.create_dataset("dates", data=candle["dates"])
                 stockgroup.create_dataset("minutes", data=candle["minutes"])
                 stockgroup.create_dataset("opens", data=candle["opens"])
@@ -174,7 +192,7 @@ class NinetoTen(Status):
         with open(pjoin(self.opt.export_to, "ninetoten.keys"), "w") as fout:
             fout.write("\n".join(stockcodes))
 
-    def _test(self):
+    def test_save(self):
         with h5py.File(pjoin(self.opt.export_to, "ninetoten.h5"), "r") as fin:
             for k in fin.keys():
                 print(k)
@@ -182,15 +200,16 @@ class NinetoTen(Status):
 
     def test(self):
         self.get_dispatch()
-        print(self.get_volume("A000660"))
+        self.get_stockcode()
+        # print(self.get_volume("A000660"))
 
     def run(self):
         self.get_dispatch()
         volume = self.get_high_volume()
         stock_map = {}
-        for pivot, stockcodes in volume.items():
+        for date, stockcodes in volume.items():
             for stockcode in stockcodes:
-                stock_map.setdefault(stockcode, []).append(pivot)
+                stock_map.setdefault(stockcode, []).append(date)
         stock_map = {k: sorted(v) for k, v in stock_map.items()}
         self.save(stock_map)
 
